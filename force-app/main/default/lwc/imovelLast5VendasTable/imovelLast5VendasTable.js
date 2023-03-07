f5e28a3f4840ff8ef716ff341a9b43697f10656b
Alex J. Silva <111905692+AlexJ1Silva@users.noreply.github.com>
2023-03-07 18:48:05 -0300

    V4.1

--- force-app/main/default/lwc/imovelLast5VendasTable/imovelLast5VendasTable.js
+++ force-app/main/default/lwc/imovelLast5VendasTable/imovelLast5VendasTable.js
@@ -14,21 +14,33 @@ export default class ImovelLast5VendasTable extends LightningElement {
 
     showSpinner = true;
 
+    
+
+
     @api imovelTO;
 
 
+    /* formatNumber(n) {
+        // format number 1000000 to 1,234,567
+        return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      } */
+
+    
     connectedCallback() {
 
         console.log(this.recordId);
         ultimas5VendasImovelCLS({ lstImovelTO: this.imovelTO }).then(r => {
 
             console.log(r);
+            //const formatNumber = new Intl.NumberFormat()
 
             if (r.length > 0) {
                
                 r.forEach(e => {
                     e.url = 'https://everymind-1e8-dev-ed.develop.lightning.force.com/lightning/r/Imovel__c/'+e.id+'/view';
                     e.cliente = 'https://everymind-1e8-dev-ed.develop.lightning.force.com/lightning/r/Contact/'+e.idCliente+'/view'
+                    //e.Valor__c = this.formatNumber(e.Valor__c);
+                   
                 });
                 this. ultimas5VendasList = r;
                 this.temDados = true;
